09c5d17de3b9def8d916194ea7dab9e506c264a3
dnedev <dimitar.nedev@me.com>
2014-04-02 17:56:54 +0200

    Add CentOS template
    Add a CentOS template
    Rename scripts to distinguish between those Fedora and CentOS

---
+++ provisioners/1-package-install-epel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+##################################################################
+# Install MonetDB, R and other required (and some useful packages)
+##################################################################
+echo "PACKAGE INSTALL"
+sudo yum install -y https://www.monetdb.org/downloads/epel/MonetDB-release-epel6-1.0-1.el6.noarch.rpm
+sudo yum install -y MonetDB-SQL-server5 MonetDB-client MonetDB-geom-MonetDB5 R vsftpd nano
