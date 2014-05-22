f587e99968292ae3ec0cc264b3671ade0b40329b
Dimitar Nedev <D.G.Nedev@cwi.nl>
2014-05-22 12:44:21 +0200

    Switch to non-testing EPEL binaries

--- provisioners/1-package-install-epel.sh
+++ provisioners/1-package-install-epel.sh
@@ -5,4 +5,4 @@
 ##################################################################
 echo "PACKAGE INSTALL"
 sudo yum install -y https://www.monetdb.org/downloads/epel/MonetDB-release-epel6-1.0-1.el6.noarch.rpm
-sudo yum --enablerepo=monetdb-testing install -y MonetDB-SQL-server5 MonetDB-client MonetDB-geom-MonetDB5 R vsftpd nano wget
+sudo yum install -y MonetDB-SQL-server5 MonetDB-client MonetDB-geom-MonetDB5 R vsftpd nano wget
