e3962d391611c54c9c1adf4c95a5d81a2f96f138
Dimitar Nedev <D.G.Nedev@cwi.nl>
2014-05-26 14:15:33 +0200

    Unify VM names in issue/motd messages

--- provisioners/96-set-issue.sh
+++ provisioners/96-set-issue.sh
@@ -2,11 +2,10 @@
 # Set welcome message with some info how to log in
 #######################################################
 # Set a new welcome banner
-echo "========================================
-MonetDB+R Analytical Database Sandbox
+echo "==============================================
+MonetDB R Analytical Database with R
 https://monetdb.org
-https://monetdbsolutions.com
-========================================
+==============================================
 To login remotely use:
 'ssh -p 2222 root@127.0.0.1'
 Default password: monetdb
--- provisioners/97-set-motd.sh
+++ provisioners/97-set-motd.sh
@@ -6,11 +6,10 @@
 echo "Set motd"
 
 echo "
-========================================
-MonetDB+R Analytical Database Appliance
-https://monetdb.org
+=============================================
+MonetDB Analytical Database Appliance with R
 https://monetdbsolutions.com
-========================================
+=============================================
 
 For first-time user setup run:
 'sh user-setup.sh'" | sudo tee -a /etc/motd > /dev/null
